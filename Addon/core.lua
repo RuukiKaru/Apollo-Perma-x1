e534352ee640da117157d487643c763397ad0087
RuukiKaru <97389948+RuukiKaru@users.noreply.github.com>
2022-01-09 12:29:00 +0000

    Delete core.lua

--- Addon/core.lua
+++
@@ -1,9 +0,0 @@
-    local frame=CreateFrame("Frame");
-    frame:RegisterEvent("PLAYER_LOGIN");
-    frame:SetScript("OnEvent",function(self,event,...)
-        SendChatMessage(".mod xp 1" ,"SAY" ,"COMMON","channel");
-    end);
-	    frame:SetScript("OnEvent",function(self,event,...)
-        SendChatMessage(".mod xp 1" ,"SAY" ,"ORCISH","channel");
-    end);
-	
